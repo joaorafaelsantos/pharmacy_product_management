382b8e9f59dda6ed6112f60fc80974caf816f997
João Santos <up201809187@fe.up.pt>
2019-12-12 11:27:58 +0000

    Added mock example for health check API

--- src/index.ts
+++ src/index.ts
@@ -10,18 +10,18 @@ const AWS_REGION = process.env.AWS_REGION
 const PORT = process.env.PORT || 3000
 const app = express()
 
-// Cors
+// CORS
 app.use(cors());
 app.options('*', cors());
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 // Swagger 
 app.use(`/api/${API_VERSION}/api-docs`, swagger)
 
 // TODO - Health check
-app.get("/", (req, res) => res.send("Product Management | Pharmacy"))
+app.get("/health", (req, res) => res.json({ status: "health", code: "200" }))
 
 // TODO - Products
 
