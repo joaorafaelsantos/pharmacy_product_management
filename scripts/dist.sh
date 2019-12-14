879fed444242ac4f520dc10b8496623430dfdd39
João Santos <up201809187@fe.up.pt>
2019-12-14 12:43:13 +0000

    Added swagger.json to build files on script

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -3,4 +3,4 @@
 } || {
   mkdir dist
 }
-zip dist/$npm_package_name.zip -r build package.json package-lock.json .env
+zip dist/$npm_package_name.zip -r build package.json package-lock.json .env swagger.json
